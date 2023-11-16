0dd0ff233d32ce8d13339088307fdc5c48f095f7
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-16 14:49:15 +0200

    massiivi arvutus

---
+++ praks1.ps1
@@ -0,0 +1,9 @@
+﻿$namearray1 = @(1, 2, 3)
+$namearray2 = @(4, 5, 6)
+
+$namearray3 = @()
+$namearray3 += $namearray1[0] + $namearray2[0]
+$namearray3 += $namearray1[1] + $namearray2[1]
+$namearray3 += $namearray1[2] + $namearray2[2]
+
+$namearray3
