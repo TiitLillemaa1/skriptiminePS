676f18e899ee57f5949f3251778bda0a298b1ff1
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 18:12:50 +0200

    practice 4

---
+++ praks4.ps1
@@ -0,0 +1,7 @@
+﻿$x1 = Read-Host "Sisesta esimene number:"
+$x2 = Read-Host "sisesta teine number:"
+
+if( $x1 -gt $x2)
+{ Write-Host "Kõrgem number on: "$x1}
+else
+{ write-host "Kõrgem number on: " $x2}
