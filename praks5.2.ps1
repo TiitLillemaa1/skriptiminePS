1a1a53afca3463417a7c310951adad9ced124e5b
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 18:42:00 +0200

    practice 5.2

---
+++ praks5.2.ps1
@@ -0,0 +1,7 @@
+﻿Get-ChildItem -Path "C:\temp\Test"
+$File = Get-ChildItem -Path "C:\temp\Test" | where {$_.Name -like ".csv"} | Select Name, Lenght
+$sizeinKB = $File.Lenght/1KB
+$sizeinMB = $file.Lenenght/1MB
+Write-Host "FileName : "$File.Name
+Write-Host "Size in KB: " $sizeinKB
+Write-Host "Size in MB: " $sizeinMB
