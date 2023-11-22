eaa55f32ea7bef6bead6bae754430dbb7734dfae
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 18:03:12 +0200

    practice 2

--- hashtable.ps1
+++ hashtable.ps1
@@ -1,9 +1,13 @@
-﻿$hash1 = @{Name="John", "Joe", "Mary";Daysworked=12,20,18}
-$hash2 = @{Name="John", "Joe", "Mary";SalaryPerDay=100,120,150}
+﻿$hashtable1 = @{Name="John", "Joe", "Mary";Daysworked=12, 20, 18}
+$hashtable2 = @{Name="John", "Joe", "Mary";SalaryPerDay=100, 120, 150}
 
-$JohnSalary = $hash1.Daysworked[0] * $hash2.SalaryPerDay[0]
-$JoeSalary = $hash1.Daysworked[1] * $hash2.SalaryPerDay[1]
-$Marysalary = $hash1.Daysworked[2] * $hash2.SalaryPerDay[2]
+$JohnSalary = $hashtable1.Daysworked[0] * $hashtable2.SalaryPerDay[0]
+$JoeSalary = $hashtable1.Daysworked[1] * $hashtable2.SalaryPerDay[1]
+$Marysalary = $hashtable1.Daysworked[2] * $hashtable2.SalaryPerDay[2]
 
-$hash3 = @{Name=$hash1.Name[0],$hash1.Name[1],$hash1.Name[2];Salary=$JohnSalary, $JoeSalary, $Marysalary}
+$hash3 = @{Name=$hashtable1.Name[0],$hashtable1.Name[1],$hashtable1.Name[2];Salary=$JohnSalary, $JoeSalary, $Marysalary}
+$hashtable1
+write-host
+$hashtable2
+write-host
 $hash3
