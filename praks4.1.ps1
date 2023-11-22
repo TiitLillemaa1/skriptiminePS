6fc993491a6ce1a8e280560a6b7f3aa31d8cc4e9
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 18:21:04 +0200

    practice 4.1

---
+++ praks4.1.ps1
@@ -0,0 +1,15 @@
+﻿Write-host "Vali riik" -ForegroundColor DarkBlue
+write-host "1 : India" -ForegroundColor Cyan
+Write-Host "2 : Australia" -ForegroundColor Cyan
+Write-Host "3 : China" -ForegroundColor Cyan
+
+$choice = Read-Host "Vali riik"
+
+if($choice -eq "1")
+{Write-host "India pealinn on New Delhi" -ForegroundColor Green}
+elseif($choice -eq "2")
+{Write-Host "Austraalia pealinn on Canberra" -ForegroundColor Green}
+elseif($choice -eq "3")
+{write-host "Hiina pealinn on Peking" -ForegroundColor Green}
+else
+{Write-Host "vale valik" -ForegroundColor Red}
