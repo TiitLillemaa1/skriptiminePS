5a52d586ac339fd3df81c93747e454ed8c2e5cf4
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 19:08:42 +0200

    practice 6

---
+++ praks6.ps1
@@ -0,0 +1,12 @@
+﻿$csv = Import-csv C:\temp\Test\Students.csv
+$result =@()
+foreach($c in $csv)
+{ if ([int]$c.Age -ge 4 -and [int]$c.Age -le 10)
+{$school = "Junior" }
+elseif([int]$c.Age -ge 11 -and [int]$c.Age -le 17)
+{$school = "Senior" }
+$temp = [PSCustomObject]@{
+Name = $c.Name
+School = $school}
+$result += $temp}
+$result
