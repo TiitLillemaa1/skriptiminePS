70c51668dcdc2cd7a73c1a8a2fba59a9a499ef72
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 20:15:23 +0200

    practice 12

---
+++ praks12.2.ps1
@@ -0,0 +1,17 @@
+﻿function countservice
+{
+param
+(
+[string]$status
+)
+if($status -eq "Running")
+{
+$Start = Get-Service | ?{$_.Status -eq "running"}
+Write-host "Total Services in running state = "$start.count}
+elseif($status -eq "Stopped")
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-host "Total services in stopped state = "$stop.count}
+}
+countservice -status Running
+countservice -status Stopped
