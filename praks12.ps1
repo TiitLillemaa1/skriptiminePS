04cef772106af4f3a64d9d4aff3e3fd062273140
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-22 20:05:48 +0200

    practice 12

---
+++ praks12.ps1
@@ -0,0 +1,13 @@
+﻿function countstart
+{
+$Start = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Total Services in running state = "$start.count
+}
+function countstop
+{
+$atop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Total Services in stopped stated = "$stop.count
+}
+countstart
+
+countstop
