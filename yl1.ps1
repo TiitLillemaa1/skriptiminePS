3d4fe9d4a1d305d0c33a0a231082d323000114b3
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-23 14:28:47 +0200

    kasutaja loomine PS skriptiga

---
+++ yl1.ps1
@@ -0,0 +1,18 @@
+﻿$eesnimi = read-host "Sisesta oma eesnimi:"
+$perenimi = read-host "Sisesta oma perekonnanimi:"
+
+$user = "$eesnimi.$perenimi"
+
+$täisnimi = "$eesnimi $perenimi"
+$Kontokirjeldus ="Kasutaja Konto: $täisnimi"
+$user = $user.ToLower()
+
+$ErrorActionPreference = 'SilentlyContinue'
+try
+{
+$password = ConvertTo-SecureString -string "Parool1!" -AsPlainText -Force
+New-LocalUser -Name $user -Password $password  -FullName "$täsinimi" -Description $Kontokirjeldus
+}
+catch
+{Write-Host "kasutaja loomine ebaõnnestus" -ForegroundColor Red
+}
