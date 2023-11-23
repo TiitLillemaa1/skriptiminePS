32acd87bde91c5af437866df400e514b1565c652
TiitLillemaa1 <tiit.lillemaa@voco.ee>
2023-11-23 14:29:21 +0200

    kasutaja eemaldamine PS skriptiga

---
+++ yl2_haldus.ps1
@@ -0,0 +1,18 @@
+﻿$eesnimi = read-host "Sisesta oma eesnimi:"
+$perenimi = read-host "Sisesta oma perekonnanimi:"
+
+$user = "$eesnimi.$perenimi"
+
+$täisnimi = "$eesnimi $perenimi"
+$Kontokirjeldus ="Kasutaja Konto: $täisnimi"
+$user = $user.ToLower()
+
+$ErrorActionPreference = 'SilentlyContinue'
+try
+{
+Remove-LocalUser -Name $user -ErrorAction Stop
+Write-Host "Kasutaja '$user' edukalt kustutatud!" -ForegroundColor Green
+}
+catch
+{Write-Host "kasutaja kustutamine ebaõnnestus" -ForegroundColor Red
+}
